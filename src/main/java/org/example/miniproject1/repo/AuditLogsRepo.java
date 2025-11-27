8d00987b6baf3cc3fc0f22c79b0a9dde6c6ed11b
Edmundus <edmundus.prasetyaji@gdn-commerce.com>
2025-11-27 14:40:20 +0700

    part1-domain

--- src/main/java/org/example/miniproject1/repo/AuditLogsRepo.java
+++
@@ -1,7 +0,0 @@
-package org.example.miniproject1.repo;
-
-import org.example.miniproject1.domain.AuditLogs;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AuditLogsRepo extends MongoRepository<AuditLogs, Integer> {
-}
