45f4cd9b43fcf8f898ac0c220932c9490eca0e77
Edmundus <edmundus.prasetyaji@gdn-commerce.com>
2025-11-27 15:05:58 +0700

    part 5 fraud client

--- src/main/java/org/example/miniproject1/external/FraudClient.java
+++ src/main/java/org/example/miniproject1/external/FraudClient.java
@@ -1,4 +1,16 @@
 package org.example.miniproject1.external;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class FraudClient {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public boolean isFraudulent(String email, String baseUrl) {
+        String url = baseUrl + "/fraud-check?email=" + email;
+        Boolean result = restTemplate.getForObject(url, Boolean.class);
+        return result != null && result;
+    }
 }
